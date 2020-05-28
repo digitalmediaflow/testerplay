83a187148f1d41d6011ab6d255609fa2f58b2ad7
Robert Romululs <digitalmediaflow@gmail.com>
2020-05-28 01:59:35 -0400

    update repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+  state = ({
+    name: '',
+    lastname: '',
+  });
+  // get field inputs
+  const getInputs = (e) => {
+    state[e.target.id] = e.target.value;
+    setstate({ ...state });
+  };
+
+  // get form data
+  const getFormData = (e) => {
+    e.preventDefaut();
+    const allstate = this.state;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <form>
+        <label>Name</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          value={this.state.name}
+          onChange={getInputs}
+        />
+        <label>Last Name</label>
+        <input
+          type='text'
+          id='lastname'
+          value={state.lastname}
+          onChange={getInputs}
+        />
+
+        <button
+          onClick={getFormData}
+          // style={{ height: '3vh', backgroundColor: 'blue', color: 'white' }}
         >
-          Learn React
-        </a>
-      </header>
+          Click Me
+        </button>
+      </form>
     </div>
   );
 }
+
+// const [state, setstate] = useState({});
+// const [genderState, setGenderState] = useState("");
+
+// const getInputs = (e)=> {
+//   state[e.target.id] = e.target.value
+//   setstate({...state});
+
+// }
+
+// const allInputs = (e) => {
+//   e.preventDefault();
+//    const allstate = {state,Gender:genderState}
 
 export default App;
